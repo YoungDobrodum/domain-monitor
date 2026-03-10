c5066aaeee132d75d7004997b308278ab21ad9c3
Houston <yung.dobrodum@gmail.com>
2026-03-10 19:00:26 +0200

    With build

---
+++ backend/start.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
+php artisan schedule:work &
+php artisan queue:work --daemon --tries=3 &
+
+exec frankenphp php-server -root public
