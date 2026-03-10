ba290069c06552f250fec064e03d96c93823cd9b
Houston <yung.dobrodum@gmail.com>
2026-03-10 21:54:57 +0200

    With build

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: '/api',
+    // baseURL: '/api',
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000/api',
     headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
